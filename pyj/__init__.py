a2e75f37b106d88fddec5ec2d601833a3393a833
William Stevens <contact@wastevensv.com>
2018-05-09 08:28:25 -0400

    Fixed warnings found with pylint and pycodestyle.

--- pyj/__init__.py
+++ pyj/__init__.py
@@ -1,7 +1,7 @@
 import jinja2 as j2
-import os.path
 import yaml
 from markdown import markdown
+
 
 class Page:
     fpath = ""
@@ -10,18 +10,22 @@ class Page:
 
     def __init__(self, fpath):
         self.fpath = fpath
-        with open(self.fpath, 'r') as f:
-            text = f.read()
-            parts = text.split('---')
-            if len(parts) == 1:
-                self.content = markdown(text)
-            else:
-                self.data = yaml.load(parts[0])
-                self.content = markdown("---".join(parts[1:]))
+
+        with open(self.fpath, 'r') as src_file:
+            text = src_file.read()
+
+        parts = text.split('---')
+        if len(parts) == 1:
+            self.content = markdown(text)
+        else:
+            self.data = yaml.load(parts[0])
+            self.content = markdown("---".join(parts[1:]))
 
     def render(self, env, parent=None, site=None):
         content_template = j2.Template(self.content)
-        content = content_template.render(self.data, parent=parent, site=site)
+        content = content_template.render(self.data,
+                                          parent=parent, site=site)
         page_template = env.get_template(self.data["template"])
-        page = page_template.render(self.data, parent=parent, site=site, content=content)
+        page = page_template.render(self.data, parent=parent,
+                                    site=site, content=content)
         return page
--- pyj/__main__.py
+++ pyj/__main__.py
@@ -1,16 +1,18 @@
 import os
-import jinja2 as j2
 from sys import argv
+
+import jinja2 as j2
 
 import pyj
 
 SITE_DIR = argv[1]
-ROOT_DIR = os.path.join(SITE_DIR,'root')
-TEMPLATES_DIR = os.path.join(SITE_DIR,'templates')
+ROOT_DIR = os.path.join(SITE_DIR, 'root')
+TEMPLATES_DIR = os.path.join(SITE_DIR, 'templates')
 
 env = j2.Environment(
-    loader = j2.FileSystemLoader(TEMPLATES_DIR),
-    autoescape = False
+    loader=j2.FileSystemLoader(TEMPLATES_DIR),
+    autoescape=False
     )
 
-print(pyj.Page(os.path.join(ROOT_DIR,"hello.md")).render(env,{},{'name':'test'}))
+print(pyj.Page(os.path.join(ROOT_DIR, "hello.md"))
+      .render(env, site={'name': 'example'}))
